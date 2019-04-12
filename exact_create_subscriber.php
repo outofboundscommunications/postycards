6aabef24d80213f67c607f68d543f85383faeeb9
jay wilner <jayw@outofboundscommunications.com>
2019-04-12 15:50:22 -0500

    comment out email ID so we don't send welcome email, change email list

--- exact_create_subscriber.php
+++ exact_create_subscriber.php
@@ -1,5 +1,5 @@
 <?php
-require('exacttarget_soap_client.php'); 
+require('exacttarget_soap_client.php');
 
 function stripslashes_deep($value) {
 	$value = is_array($value) ?
@@ -33,43 +33,44 @@ try {
         $subscriber = new ExactTarget_Subscriber();
         $subscriber->EmailAddress = $email_address;
         $subscriber->SubscriberKey = $email_address;
-       
-        $subscriber->Lists = array();   
-    
+
+        $subscriber->Lists = array();
+
         $ExampleAttribute1 = new ExactTarget_Attribute();
         $ExampleAttribute1->Name = "First Name";
-        $ExampleAttribute1->Value = $first_name;   
+        $ExampleAttribute1->Value = $first_name;
 
         $ExampleAttribute2 = new ExactTarget_Attribute();
         $ExampleAttribute2->Name = "Last Name";
         $ExampleAttribute2->Value = $last_name;
 
-        $subscriber->Attributes=array($ExampleAttribute1,$ExampleAttribute2);      
+        $subscriber->Attributes=array($ExampleAttribute1,$ExampleAttribute2);
 
         $list = new ExactTarget_SubscriberList();
-        $list->ID = "3950784"; // This is the ID of the subscriber list             
+        //$list->ID = "3950784"; // This is the ID of the subscriber list
+				$list->ID = "4693143"; // This is the ID of the new 'pop up unbounce' email list
         $subscriber->Lists[] = $list;
 
         // This is the section needed to define it as an "Upsert"
         $so = new ExactTarget_SaveOption();
         $so->PropertyName = "*";
-        $so->SaveAction = ExactTarget_SaveAction::UpdateAdd;            
-        $soe = new SoapVar($so, SOAP_ENC_OBJECT, 'SaveOption', "http://exacttarget.com/wsdl/partnerAPI");            
-        $opts = new ExactTarget_UpdateOptions();            
+        $so->SaveAction = ExactTarget_SaveAction::UpdateAdd;
+        $soe = new SoapVar($so, SOAP_ENC_OBJECT, 'SaveOption', "http://exacttarget.com/wsdl/partnerAPI");
+        $opts = new ExactTarget_UpdateOptions();
         $opts->SaveOptions = array($soe);
-            
+
         // Below are examples of updating the subscriber status to active or unsub
         //$subscriber->Status = ExactTarget_SubscriberStatus::Active;
         //$subscriber->Status = ExactTarget_SubscriberStatus::Unsubscribed;
 
         $object = new SoapVar($subscriber, SOAP_ENC_OBJECT, 'Subscriber', "http://exacttarget.com/wsdl/partnerAPI");
-            
+
         $request = new ExactTarget_CreateRequest();
         $request->Options = $opts;
-        $request->Objects = array($object);            
-		
-        $results = $client->Create($request);   
-       
+        $request->Objects = array($object);
+
+        $results = $client->Create($request);
+
 		$key="PHP_Test_".uniqid();
 		///email sending from here
 		//Setup the Email Send Definition
@@ -85,87 +86,87 @@ try {
 		// Setting Up the Source List
 		$emailSendDef->SendDefinitionList = array();
 		$sendDefList = new ExactTarget_SendDefinitionList();
-		
+
 		$list = new ExactTarget_List();
 		$list->ID = "3950784";
 		$sendDefList->List = $list;
 		$sendDefList->DataSourceTypeID = "List";
 		$sendDefList->SendDefinitionListType = "SourceList";
-		$emailSendDef->SendDefinitionList[] = $sendDefList;	
-		
+		$emailSendDef->SendDefinitionList[] = $sendDefList;
+
 
 		// Specify the Email To Send
 		$email = new ExactTarget_Email();
-		$email->ID = "4589853"; // Email702-LandingPageOptIn  4498674"
+		//$email->ID = "4589853"; // Email702-LandingPageOptIn  4498674"
 		$emailSendDef->Email = $email;
-		
-		
+
+
 		$object = new SoapVar($emailSendDef, SOAP_ENC_OBJECT, 'EmailSendDefinition', "http://exacttarget.com/wsdl/partnerAPI");
 		$request = new ExactTarget_CreateRequest();
 		$request->Options = NULL;
-		$request->Objects = array($object);		
+		$request->Objects = array($object);
 
 		//$results = $client->Create($request);
-		
-		
-	
+
+
+
 		$tsd = new ExactTarget_TriggeredSendDefinition();
 		$tsd->CustomerKey = $key; // unique identifier for the triggered send definition
 		$tsd->Name = $tsd->CustomerKey; // set the name to be the same as the customer key
-				
+
 		// Define the email to be sent
 		$tsd->Email = new ExactTarget_Email(); // create email object to attach to the send
-		$tsd->Email->ID = "4589853"; // Email702-LandingPageOptIn  4498674; // id of the email you want to attach to the triggered send
-		
+		//$tsd->Email->ID = "4589853"; // Email702-LandingPageOptIn  4498674; // id of the email you want to attach to the triggered send
+
 		// Define the send classification that is associated with this send
 		$sc = new ExactTarget_SendClassification();
 		$sc->CustomerKey = "Default Commercial"; // external key for the send classification we want to use
 		$tsd->SendClassification = $sc; // create send classification object to attach to the send
-		
+
 		// Send the email as multipart mime
 		$tsd->IsMultipart = true; // Send as Multipart MIME
-				
+
 		// Create the object
 		$object = new SoapVar($tsd, SOAP_ENC_OBJECT, 'TriggeredSendDefinition', "http://exacttarget.com/wsdl/partnerAPI");
-				
+
 		// Create the triggered send definition
 		$request = new ExactTarget_CreateRequest();
 		$request->Options = NULL;
 		$request->Objects = array($object);
-		$results = $client->Create($request);		
-	
-		
-		
-				$ts = new ExactTarget_TriggeredSend(); 
-				//$ts->CustomerKey = $key; 
-                $tsd = new ExactTarget_TriggeredSendDefinition(); 
-                $tsd->CustomerKey = $key; 
+		$results = $client->Create($request);
+
+
+
+				$ts = new ExactTarget_TriggeredSend();
+				//$ts->CustomerKey = $key;
+                $tsd = new ExactTarget_TriggeredSendDefinition();
+                $tsd->CustomerKey = $key;
 				$tsd->TriggeredSendStatus = ExactTarget_TriggeredSendStatusEnum::Active  ;
 				$object = new SoapVar($tsd, SOAP_ENC_OBJECT, 'TriggeredSendDefinition', "http://exacttarget.com/wsdl/partnerAPI");
-				 
+
 				$request = new ExactTarget_UpdateRequest();
                 $UpdateOption = new ExactTarget_UpdateOptions();
                       // Apply options and object to request
 				$request->Options = new SoapVar($UpdateOption, SOAP_ENC_OBJECT, 'UpdateOptions', "http://exacttarget.com/wsdl/partnerAPI");
 				$request->Objects = array($object);
-                               
+
                                 // Execute the CreateRequest
 				$results = $client->Update($request);
-				 
+
                 $ts->Subscribers = $subscriber;
                 $ts->TriggeredSendDefinition = $tsd;
-                
+
                 $object = new SoapVar($ts, SOAP_ENC_OBJECT, 'TriggeredSend', "http://exacttarget.com/wsdl/partnerAPI");
 
                 //var_dump($object);
-                
+
 			    $request = new ExactTarget_CreateRequest();
 			    $request->Options = NULL;
 			    $request->Objects = array($object);
-				
+
 				$results = $client->Create($request);
-				
-		
+
+
 }
 catch (SoapFault $e) {
     /* output the resulting SoapFault upon an error */
@@ -180,7 +181,7 @@ $client->__getLastRequest() ."\n";
 $client->__getLastResponse()."\n\r\n\r\n";
 
 
-$message_body = 
+$message_body =
 <<<EOM
 Email: $email_address \n
 first_name: $first_name \n
